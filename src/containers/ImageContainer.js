b93b40e55f9a334a515765d2b539c06eec32be64
Gia Edgington <giaedgington@Erics-MacBook.local>
2019-08-20 10:55:42 -0400

    commented out imageContainer

--- src/containers/ImageContainer.js
+++ src/containers/ImageContainer.js
@@ -3,18 +3,18 @@ import Image from '../components/Image';
 
 export default class Homepage extends Component {
 
-    makeInstructorCards = () => {
-        let allCards = this.props.allCards
-        return allCards.filter(card => {
-            card.filter === "instructor" 
-            return <Image image={card.image}/>
-        })
-    }
+    // makeInstructorCards = () => {
+    //     let allCards = this.props.allCards
+    //     return allCards.filter(card => {
+    //         card.filter === "instructor"
+    //         return <Image image={card.image}/>
+    //     })
+    // }
 
   render(){
     return(
       <div>
-        {this.makeInstructorCards()}
+        {/* {this.makeInstructorCards()} */}
       </div>
     )
   }
