61631f0a6042b24bd7c4dfc1e75d97f326e83201
Kevin Lai <kevinlai76@gmail.com>
2019-08-20 10:53:20 -0400

    filterType State added to App

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -3,15 +3,20 @@ import { Link } from 'react-router-dom';
 
 export default class Filters extends React.Component {
 
+    handleClick = (e) =>{
+        this.props.changeFilterTypeState(e)
+        
+    }
+
     render(){
         return(
             <div>
                 <ul className="filters">
-                  <li>Instructors</li>
-                  <li>Mod 1</li>
-                  <li>Mod 2</li>
-                  <li>Mod 3</li>
-                  <li>Mod 4</li>
+                  <li id="instructor" onClick={this.handleClick}>Instructors</li>
+                  <li id="mod1" onClick={this.handleClick}>Mod 1</li>
+                  <li id="mod2" onClick={this.handleClick}>Mod 2</li>
+                  <li id="mod3" onClick={this.handleClick}>Mod 3</li>
+                  <li id="mod4" onClick={this.handleClick}>Mod 4</li>
                   <Link to="/game_board"><button>Start Game</button> </Link>
                 </ul>
             </div>
