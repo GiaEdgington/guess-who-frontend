02437c47d8ee92534c7cedeb42b2bdf0bc6d6801
Kevin Lai <kevinlai76@gmail.com>
2019-08-21 18:25:04 -0400

    conflict manager

--- src/components/Input.js
+++ src/components/Input.js
@@ -2,13 +2,34 @@ import React from 'react';
 
 export default class Input extends React.Component {
 
-    render(){
+    state = {
+        input: "",
 
+    }
+
+    handleChange = (event) => {
+        // console.log(event.target.value)
+        if (this.props.seconds !== 0) {
+            this.setState({
+                input: event.target.value
+            })
+        }
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault()
+        this.props.getInput(this.state.input)
+        // this.props.setScore()
+    }
+
+    render(){
+        // console.log(this.state.input)
+        // console.log('input', this.props.seconds)
         return(
             <div>
-                <input className="input">
-
-                </input>
+                <form onSubmit={this.handleSubmit}>
+                    <input className="input" value={this.state.input} onChange={this.handleChange}/>
+                </form>
             </div>
         )
     }
--- src/containers/GameContainer.js
+++ src/containers/GameContainer.js
@@ -5,12 +5,14 @@ import GameBoard from './GameBoard'
 export default class GameContainer extends React.Component {
 
    state = {
-       seconds: 0
+       seconds: 0,
+       gameInSession: true
    }
 
    intervalHandle;
 
    tick = () => {
+    if (this.state.gameInSession){
      let secondsRemaining = this.state.seconds
      let newState = secondsRemaining - 1
        this.setState({
@@ -20,22 +22,32 @@ export default class GameContainer extends React.Component {
        if (this.state.seconds === 0) {  //this.seconds? anyway, however you get the state value. same with the above seconds-- line
            clearInterval(this.intervalHandle)
        }
+    }
    }
 
    handleTimer = () => {
      if (this.state.seconds === 0) {
-        this.setState({ seconds: 45 })
+        this.setState({ seconds: 30 })
         this.intervalHandle = setInterval(this.tick, 1000);
      }
    }
 
+   checkGameState = () => {
+       this.setState({gameInSession: false})
+   }
 
    render() {
-       console.log(this.state.seconds)
+       console.log(this.state.gameInSession)
        return (
            <React.Fragment>
               <SideBar changeFilterTypeState= { this.props.changeFilterTypeState } />
-              <GameBoard allCards={ this.props.allCards } handleTimer = { this.handleTimer } seconds = { this.state.seconds } />
+              <GameBoard 
+                allCards={ this.props.allCards } 
+                handleTimer = { this.handleTimer } 
+                seconds = { this.state.seconds } 
+                checkGameState={this.checkGameState}
+                gameState={this.gameInSession}
+              />
            </React.Fragment>
        )}
 }
