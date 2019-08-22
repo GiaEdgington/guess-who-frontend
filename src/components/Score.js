fcd6fa5ae5a31536f168759fb8210a3b5e6c0697
Kevin Lai <kevinlai76@gmail.com>
2019-08-22 10:14:12 -0400

    working score

--- src/components/Score.js
+++ src/components/Score.js
@@ -2,17 +2,12 @@ import React from 'react';
 
 export default class Score extends React.Component {
 
-    state = {
-        score: 0
-    }
-
-
     render(){
         //console.log('score component: ', this.props.seconds)
         //console.log(this.state.score)
         return(
             <div className="score">
-                MY SCORE: {this.props.seconds}
+                MY SCORE: {this.props.updateScore}
             </div>
         )
     }
--- src/containers/GameBoard.js
+++ src/containers/GameBoard.js
@@ -10,6 +10,7 @@ export default class GameBoard extends React.Component {
 
     state = {
         input: "",
+        score: 0
         //answer: "Kevin"
         // remember to change this to an empty string
         // remember to pass the function back up to change this stupid shit
@@ -28,21 +29,34 @@ export default class GameBoard extends React.Component {
         }
     }
 
+    // this.setState(prevState => ({
+    //     isToggleOn: !prevState.isToggleOn
+    //   }));
+
     getInput = (userInput) => {
         if(userInput.toLowerCase() === this.props.answer.toLowerCase()){
             this.props.checkGameState()
+            this.setState({
+                score: this.state.score + 10
+            });
         }
     }
 
+    clearInputAfterSubmit = () => {
+        this.setState({
+            input: ""
+        })
+    }
+
     render(){
-        //console.log(this.props)
+        // console.log(this.state.score)
         return(
             <div>
-                <Score gameState={this.gameInSession} seconds={this.props.seconds}/>
+                <Score gameState={this.gameInSession} seconds={this.props.seconds} updateScore={this.state.score}/>
                 <Timer seconds={this.props.seconds}/>
                 <Hint />
                 <ImageContainer allCards={this.props.allCards} filterType={this.props.filterType} setAnswer={this.setAnswer} index={this.props.index} indexImg = {this.props.indexImg}/>
-                <Input getInput={this.getInput} seconds={this.props.seconds} input={this.state.input} handleChange={this.handleChange}/>
+                <Input getInput={this.getInput} seconds={this.props.seconds} input={this.state.input} handleChange={this.handleChange} clearInputAfterSubmit={this.clearInputAfterSubmit}/>
                 <button className="btnStart" style={{ fontSice:300}} onClick={(event) => {this.props.handleTimer(); this.props.handleImage()}}>START GAME</button>
             </div>
         )
